75eb905df4dc470b18d1e816dd3305da26c8f2d2
dee20 <dee20@ic.ac.uk>
2021-08-07 13:03:48 +0100

    Debugged and fixed intersectsWith function. Kobi and David did this together

--- lib/goomap.dart
+++ lib/goomap.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'package:tuple/tuple.dart';
 import 'segment.dart';
 import 'package:collection/collection.dart';
 import 'package:maps_toolkit/maps_toolkit.dart' as mp;
@@ -53,26 +54,33 @@ class _GooMapState extends State<GooMap> {
           style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
-      visible: polygonLatLngs.length >= 4 && isPolygonSimple(polygonLatLngs),
+      visible: polygonLatLngs.length >= 4,
     );
   }
 
   // Check if Polygon is simple
   // TODO: Complete isPolygonSimple function following steps below.
-  bool isPolygonSimple(List<LatLng> poly) {
-    List<Segment> lines;
-    for (var i = 0; i < poly.length; i += 2) {
-      var line = new Segment(poly[i], poly[i + 1]);
-      lines.add(line);
-      if (lines.length != 0) {
-        for (var j = 0; j < lines.length; j++) {
-          if (line.intersectsWith(lines[j])) {
-            return false;
-          }
+  Tuple2 isPolygonSimple(List<LatLng> poly) {
+    // -1 means that the 2nd element of the tuple is invalid
+    if (poly.length < 4) {
+      Tuple2 hi = Tuple2<bool, int>(true, -1);
+      return hi;
+    }
+
+    // Initialise the lines list
+    List<Segment> lines = [];
+    lines.add(Segment(poly[poly.length - 1], poly[0]));
+    for (int i = 0; i < poly.length - 1; i++) {
+      Segment line = Segment(poly[i], poly[i + 1]);
+      if (lines.length > 1) {
+        for (int j = 0; j < lines.length; j++) {
+          if (line.intersectsWith(lines[j])) return Tuple2(false, j);
         }
       }
+      lines.add(line);
     }
-    return true;
+
+    return Tuple2(true, -1);
 
     // 1) Create a list of all of the line segments in the polygon.
     // Every consecutive pair from the list forms a line segment.
@@ -81,8 +89,18 @@ class _GooMapState extends State<GooMap> {
     // 2) Check for each line segment whether it intersects with any of the other
     // line segments from the
     // Use the intersectsWith method from the Segment class.
-    // 3) If any of the line segements do intersect with a 3rd line return false,
+    // 3) If any of the line segements do intersect with a line return false,
     // otherwise return true.
+  }
+
+  void redrawPolygon(List<LatLng> poly, int changeCoordPos) {
+    // The most recent and penultimate point form a segment that intersects
+    // The last point becomes the (changeCoordPos)th point and points after the (change) point
+    // Are shifted by one to the right.
+
+    LatLng lastPoint = poly[polygonLatLngs.length - 1];
+    poly.insert(changeCoordPos, lastPoint);
+    //for (int i = changeCoordPos; i < polygonLatLngs.length - 1; i++) {}
   }
 
   // Draw this type of Polygon to the map if it has less than 3 points.
@@ -158,17 +176,42 @@ class _GooMapState extends State<GooMap> {
             if (_isPolygon) {
               setState(() {
                 polygonLatLngs.add(point);
-                if (polygonLatLngs.length == 4) {
-                  _polygonIdCounter++;
+
+                Tuple2 isSimpleTuple = isPolygonSimple(polygonLatLngs);
+
+                bool isSimple = isSimpleTuple.item1;
+                int pos = isSimpleTuple.item2;
+                print("************LIST: $polygonLatLngs");
+                print("************TUPLE_ITEM1: $isSimple");
+                print("************TUPLE_ITEM2: $pos");
+
+                // This is the position of the start of the line segment which
+                // intersects with the last line segment.
+                int changeCoordPos;
+                if (isSimpleTuple.item2 != -1) {
+                  int changeCoordPos = isSimpleTuple.item2;
                 }
+
+                // If the Polygon is not simple we can redraw the Polygon
+                // to create an enclosed shape and use it instead.
+                if (isSimple) {
+                  _setSimplePolygon();
+                } else {
+                  // redrawPolygon(polygonLatLngs, changeCoordPos);
+                  print("*******POLYGON IS NOT SIMPLE");
+                  _setSimplePolygon();
+                }
+                /*print("---------------------------------------");
+                print("The polygon has no self-intersecting lines: $isSimple");
                 print(polygonLatLngs);
+                print("---------------------------------------");
                 if (polygonLatLngs.length < 4) {
                   _setBasicPolygon();
-                } else if (isPolygonSimple(polygonLatLngs)) {
+                } else if (isPolygonSimple(polygonLatLngs).item1) {
                   _setSimplePolygon();
                 } else {
                   _setInvalidPolygon();
-                }
+                }*/
               });
             }
           },
--- lib/segment.dart
+++ lib/segment.dart
@@ -10,7 +10,12 @@ class Segment {
   LatLng _start;
   LatLng _end;
 
-  Segment(this._start, this._end);
+  Segment(LatLng start, LatLng end) {
+    assert(start != null);
+    assert(end != null);
+    _start = start;
+    _end = end;
+  }
 
   LatLng getStartPoint() {
     return _start;
@@ -20,49 +25,26 @@ class Segment {
     return _end;
   }
 
-  // Returns true if the given line segment intersects with the
-  // other line segment given as an argument.
+  @override
+  String toString() {
+    String start = _start.toString();
+    String end = _end.toString();
+    // TODO: implement toString
+    return "Segment($start, $end)";
+  }
+
+  bool CCW(LatLng p1, LatLng p2, LatLng p3) {
+    return (p3.latitude - p1.latitude) * (p2.longitude - p1.longitude) >
+        (p2.latitude - p1.latitude) * (p3.longitude - p1.longitude);
+  }
+
   bool intersectsWith(Segment other) {
-    // If there is a point of intersection it will lie within an interval, ia,
-    ExclusiveInterval ia;
-    // We must first check that this interval exists.
-    if (max(_start.latitude, _end.latitude) <
-        max(other._start.latitude, other._end.latitude)) {
-      return false;
-    } else {
-      ia = ExclusiveInterval(
-          max(min(_start.latitude, _end.latitude),
-              min(other._start.latitude, other._end.latitude)),
-          min(max(_start.latitude, _end.latitude),
-              max(other._start.latitude, other._end.latitude)));
-    }
+    LatLng p1 = _start;
+    LatLng p2 = _end;
+    LatLng p3 = other._start;
+    LatLng p4 = other._end;
 
-    // Now we have two line formulas, with mutual intervals:
-    // f1(x) = y = a1*x + b1
-    // f2(x) = y = a2*x + b2
-    double a1 =
-        (_start.longitude - _end.longitude) / (_start.latitude - _end.latitude);
-    double a2 = (other._start.longitude - other._end.longitude) /
-        (other._start.latitude - other._end.latitude);
-    double b1 = (_start.longitude) - a1 * (_start.latitude);
-    double b2 = (other._start.longitude) - a2 * (other._start.latitude);
-
-    // If the segments are parallel, then they will not intersect.
-    // a1 and a2 correspond to the gradients of the lines
-    if (a1 == a2) {
-      return false;
-    }
-
-    // Let the potential point of intersection be (xa, ya).
-    double xa = (b2 - b1) / (a1 - a2);
-
-    // This point of intersection is still 'potential' because it may
-    // not be within the interval ia.
-    // Below we check that xa is within the mutual interval.
-    if (ia.contains(xa)) {
-      return true;
-    } else {
-      return false;
-    }
+    return (CCW(p1, p3, p4) != CCW(p2, p3, p4)) &&
+        (CCW(p1, p2, p3) != CCW(p1, p2, p4));
   }
 }
