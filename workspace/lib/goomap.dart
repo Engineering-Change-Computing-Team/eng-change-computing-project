b3a486944a4db84526bc31de8b335325fb44bb9f
dee20 <dee20@ic.ac.uk>
2021-09-13 13:19:01 +0100

    Fixed the custom button that moves you back to your current location

--- workspace/lib/goomap.dart
+++ workspace/lib/goomap.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:collection';
 import 'package:tuple/tuple.dart';
 import 'segment.dart';
@@ -19,6 +20,8 @@ class GooMap extends StatefulWidget {
 class _GooMapState extends State<GooMap> {
   // Location
   LocationData _locationData;
+  Completer _controller = Completer();
+  GoogleMapController mapController;
 
   //Polygon initial set up
   Set<Polygon> _polygons = HashSet();
@@ -36,7 +39,20 @@ class _GooMapState extends State<GooMap> {
     _locationData = widget.location;
   }
 
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
+  void locationUpdate() {
+    print("-----------------Entered Location fun");
+    mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: LatLng(_locationData.latitude, _locationData.longitude),
+            zoom: 16),
+      ),
+    );
+  }
 
   int getIntersectionIndex(List<LatLng> poly) {
     Segment lastLine = Segment(poly.last, poly[0]);
@@ -58,27 +74,6 @@ class _GooMapState extends State<GooMap> {
     }
     return -1;
   }
-
-  // The results button that will appear if an appropiate Polygon is selected
-  // Right now, the Polygon is 'appropriate' if it has 4 or more points.
-  // TODO: (Backend) make 'appropriate' - 4 or more points and an enclosed area.
-  /*Visibility resultsButton() {
-    // TODO: Complete the design of the results button.
-    return Visibility(
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(primary: Colors.lightBlueAccent),
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => ResultPage()));
-        },
-        child: Text(
-          'See results',
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-      ),
-      visible: polygonLatLngs.length >= 4,
-    );
-  }*/
 
   // Check if Polygon is simple
   // TODO: Complete isPolygonSimple function following steps below.
@@ -150,53 +145,54 @@ class _GooMapState extends State<GooMap> {
         fillColor: Colors.red.withOpacity(0.15)));
   }
 
-Widget buildFloatingSearchBar() {
-        final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
-        return FloatingSearchBar(
-          hint: 'Search...',
-          scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-          transitionDuration: const Duration(milliseconds: 800),
-          transitionCurve: Curves.easeInOut,
-          physics: const BouncingScrollPhysics(),
-          axisAlignment: isPortrait ? 0.0 : -1.0,
-          openAxisAlignment: 0.0,
-          width: isPortrait ? 600 : 500,
-          debounceDelay: const Duration(milliseconds: 500),
-          onQueryChanged: (query) {
-            // Call your model, bloc, controller here.
-          },
-          // Specify a custom transition to be used for
-          // animating between opened and closed stated.
-          transition: CircularFloatingSearchBarTransition(),
-          actions: [
-            FloatingSearchBarAction(
-              showIfOpened: false,
-              child: CircularButton(
-                icon: const Icon(Icons.place),
-                onPressed: () {},
-              ),
+  Widget buildFloatingSearchBar() {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+        // Call your model, bloc, controller here.
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: Colors.white);
+              }).toList(),
             ),
-            FloatingSearchBarAction.searchToClear(
-              showIfClosed: false,
-            ),
-          ],
-          builder: (context, transition) {
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Material(
-                color: Colors.white,
-                elevation: 4.0,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: Colors.accents.map((color) {
-                    return Container(height: 112, color: Colors.white);
-                  }).toList(),
-                ),
-              ),
-            );
-          },
+          ),
         );
-      }
+      },
+    );
+  }
 
   // Removes the last point set at the Polygon
   Widget _removePolygonPoint() {
@@ -223,8 +219,10 @@ Widget buildFloatingSearchBar() {
             zoom: 16,
           ),
           mapType: MapType.hybrid,
+          onMapCreated: _onMapCreated,
           polygons: _polygons,
           myLocationEnabled: true,
+          myLocationButtonEnabled: false,
           onTap: (point) {
             print("-----------------------$_isPolygon");
             if (_isPolygon) {
@@ -273,90 +271,87 @@ Widget buildFloatingSearchBar() {
         ),
         // LAYER OF BUTTONS
         //Button 1 (get Location)
-        /*  Positioned(
-            right: 15,
-            top: 100,
-            child: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 30,
-                    child: IconButton(
-                      color: Color(0xff91C07C),
-                      icon: Icon(Icons.gps_not_fixed),
-                      iconSize: 38.0,
-                      // alignment: Alignment.centerRight,
+        Positioned(
+          right: 15,
+          top: 100,
+          child: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 30,
+              child: IconButton(
+                  color: Color(0xff91C07C),
+                  icon: Icon(Icons.gps_not_fixed),
+                  iconSize: 38.0,
+                  // alignment: Alignment.centerRight,
 
-                      onPressed: _currentLocation,
-                    )),
-          ), */
-          //Button 2 (Pencil)
-          Positioned(
-            right: 15,
-            top: 170,
-            child: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 30,
-                    child: IconButton(
-                      color: Color(0xff91C07C),
-                      icon: Icon(Icons.create_outlined),
-                      iconSize: 38.0,
-                      // alignment: Alignment.centerRight,
+                  onPressed: locationUpdate)),
+        ),
+        //Button 2 (Pencil)
+        Positioned(
+          right: 15,
+          top: 170,
+          child: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 30,
+              child: IconButton(
+                color: Color(0xff91C07C),
+                icon: Icon(Icons.create_outlined),
+                iconSize: 38.0,
+                // alignment: Alignment.centerRight,
 
-                      onPressed: () {
-                        _isPolygon = true;
-                      },
-                    )),
-          ),
-          
-          //Button 3 (Undo Point)
-          Positioned(
-            right: 15,
-            top: 240,
-            child: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 30,
-                    child: IconButton(
-                      color: Color(0xff91C07C),
-                      icon: Icon(Icons.undo),
-                      iconSize: 38.0,
-                      // alignment: Alignment.centerRight,
+                onPressed: () {
+                  _isPolygon = true;
+                },
+              )),
+        ),
 
-                      onPressed: () {
-                        if (polygonLatLngs.length > 0) {
-                          setState(() {
-                            polygonLatLngs.removeLast();
-                          });
-                        }
-                      },
-                    )),
-          ),
-          Align(
+        //Button 3 (Undo Point)
+        Positioned(
+          right: 15,
+          top: 240,
+          child: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 30,
+              child: IconButton(
+                color: Color(0xff91C07C),
+                icon: Icon(Icons.undo),
+                iconSize: 38.0,
+                // alignment: Alignment.centerRight,
+
+                onPressed: () {
+                  if (polygonLatLngs.length > 0) {
+                    setState(() {
+                      polygonLatLngs.removeLast();
+                    });
+                  }
+                },
+              )),
+        ),
+        Align(
             alignment: Alignment.bottomCenter,
             child: TextButton(
-              onPressed: () {
-                if (polygonLatLngs.length >= 4) {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => ResultPage(_locationData)));
-                }
-              },
-              child: Container(
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.7,
-                    height: MediaQuery.of(context).size.height * 0.075,
-                    child: Center(
-                        child: Text(
-                      "Measure",
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                      textAlign: TextAlign.center,
-                    )),
-                  ),
-                  decoration: BoxDecoration(
-                      color: Color(0xff91C07C),
-                      borderRadius: BorderRadius.all(Radius.circular(15))
-                      )
-                      )
-                      )
-          ),
-          buildFloatingSearchBar(),
+                onPressed: () {
+                  if (polygonLatLngs.length >= 4) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ResultPage(_locationData)));
+                  }
+                },
+                child: Container(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      height: MediaQuery.of(context).size.height * 0.075,
+                      child: Center(
+                          child: Text(
+                        "Measure",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                        textAlign: TextAlign.center,
+                      )),
+                    ),
+                    decoration: BoxDecoration(
+                        color: Color(0xff91C07C),
+                        borderRadius: BorderRadius.all(Radius.circular(15)))))),
+        buildFloatingSearchBar(),
       ]),
     );
   }
